43981243dfcf7d897f09e43a77c504302ff90217
marutpong <marutpong@users.noreply.github.com>
2016-12-20 16:19:18 +0700

    Update gogod_install.sh

--- gogod_install.sh
+++ gogod_install.sh
@@ -103,7 +103,7 @@ fi
 
 wget $gogod_url -O $temp_path/gogod.tar.gz
 tar -xzf $temp_path/gogod.tar.gz -C $temp_path
-mv $temp_path/gogod-master-* $temp_path/gogod
+mv $temp_path/gogo-daemon-* $temp_path/gogod
 
 if [ -f "$service_file" ]
 then
